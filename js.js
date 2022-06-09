1d9f0663bf504300405f5255b3e4518b6524c037
sarda <juliensarda1408@gmail.com>
2022-06-09 10:41:18 +0200

    work section

--- js.js
+++ js.js
@@ -7,9 +7,23 @@ $('.navTrigger').click(function () {
 
 });
 
-window.setTimeout(function(){
-  $('.skill-progress').addClass("go");
-}, 1000);
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
 // navbar
 
 // var slideIndex = 1;
